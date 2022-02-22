e20c857afcbbba413febe69744fef2addf41fdfc
viola951019 <100200503+viola951019@users.noreply.github.com>
2022-02-22 19:43:06 +0800

    Create ex.cpp

---
+++ ex.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 15;
+  int myHeight = 170;
+  int myWeight = 60;
+  int mybrother = 1 ;
+  printf(" I am %d olds,\n ", myAge); 
+  printf(" my height is %d,\n", myHeight);
+  printf(" my weight %d,\n", myWeight);
+  printf(" I have %d brother.", mybrother);
+  return 0;
+}
